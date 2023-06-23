4eb9b5bcede7d2531af2f86ed909b4ec38874e0d
«d16» <s.ka@yandex.ru>
2023-06-23 16:06:33 +0300

    task_8 принимает на вход число и проверяет, кратно ли оно

---
+++ task_8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+
+void PrintMultipy (int num)
+{
+if (num % 7 == 0 && num % 23 == 0)
+{
+System.Console.WriteLine ("Да");
+
+}
+else
+{
+    System.Console.WriteLine ("Нет");
+}
+
+}
+System.Console.Write("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintMultipy (number);
