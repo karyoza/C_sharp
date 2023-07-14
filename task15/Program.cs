cd65d156fbfb6220b226b65da9d3f119521d3367
«d16» <s.ka@yandex.ru>
2023-07-14 17:08:05 +0300

    task 15 задача 26

---
+++ task15/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int CountDigit(int num)
+{
+  int count = 0;
+  while (num > 0)
+  {
+    num/=10;
+    count++;
+  }
+  return count;
+}
+Console.WriteLine("Введите число:");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if(CountDigit(number) == 0)
+{
+    Console.WriteLine("1");
+}
+else 
+{
+    Console.WriteLine(CountDigit(number));
+}
+
+
+
