3778cf4afd7a50dd467c988c410e84d51137f786
«d16» <s.ka@yandex.ru>
2023-07-19 17:14:26 +0300

    задача 40 3 числа - треугольник тру и фолс

---
+++ task24/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет,
+//  может ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+
+bool ChekLength (int a, int b, int c)
+{
+    if (a < b+c && b < a+c && c < a + b  )
+    {
+        return true;
+    }
+    else return false;
+}
+
+Console.Write("Введите число 1 : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 : ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3 : ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+ChekLength(a, b, c);
+Console.Write(ChekLength(a, b, c));
+
